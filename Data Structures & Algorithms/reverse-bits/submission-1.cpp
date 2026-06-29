efe4edd835b2747259a143393bc1c67bb0010ae4
Divyesh Jain <42700209+BlitW0@users.noreply.github.com>
2026-06-29 13:04:27 -0700

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = n;
+        ans = (ans >> 16) | (ans << 16);
+        ans = ((ans & 0xff00ff00) >> 8) | ((ans & 0x00ff00ff) << 8);
+        ans = ((ans & 0xf0f0f0f0) >> 4) | ((ans & 0x0f0f0f0f) << 4);
+        ans = ((ans & 0xcccccccc) >> 2) | ((ans & 0x33333333) << 2);
+        ans = ((ans & 0xaaaaaaaa) >> 1) | ((ans & 0x55555555) << 1);
+        return ans;
+    }
+};
