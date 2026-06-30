79267d5f7cbc11efd663212d9d58f5872d42715c
Divyesh Jain <42700209+BlitW0@users.noreply.github.com>
2026-06-29 19:12:18 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector <vector <int> > g(numCourses, vector <int> ());
+        vector <int> indegree(numCourses, 0);
+
+        for (auto e : prerequisites) {
+            int u = e[1], v = e[0];
+            g[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue <int> bfs;
+        for (int i = 0; i < numCourses; i++)
+            if (not indegree[i])
+                bfs.push(i);
+        
+        vector <int> order;
+        while (not bfs.empty()) {
+            int u = bfs.front();
+            bfs.pop();
+            order.push_back(u); // visited, add to order
+
+            for (auto v : g[u])
+                if (not --indegree[v]) // add only when course has no prereqs left, u taken so reduce prereq for v by 1
+                    bfs.push(v);
+        }
+        if (order.size() != numCourses)
+            order.clear();
+
+        return order;
+    }
+};
