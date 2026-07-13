9d4cbd43e947fef15e5ff8c28c7d8351f9df273f
Divyesh Jain <42700209+BlitW0@users.noreply.github.com>
2026-07-12 19:12:57 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,15 @@
+class TimeMap {
+    unordered_map <string, map <int, string> > store;
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        store[key].insert( {timestamp, value} );
+    }
+    
+    string get(string key, int timestamp) {
+        // first val greater than x
+        auto it = store[key].upper_bound(timestamp);
+        return it == store[key].begin() ? "" : prev(it)->second;
+    }
+};
